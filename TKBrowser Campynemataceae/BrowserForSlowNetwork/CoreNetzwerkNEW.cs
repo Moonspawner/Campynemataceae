de280a1096ac0e203ab2dc648e03527bc81d4533
Alexmitter <compaq@live.de>
2015-05-02 17:28:03 +0200

    Ver. 0.20.8
    Magic on CoreNetzwerkNew

--- TKBrowser Campynemataceae/BrowserForSlowNetwork/CoreNetzwerkNEW.cs
+++ TKBrowser Campynemataceae/BrowserForSlowNetwork/CoreNetzwerkNEW.cs
@@ -13,21 +13,25 @@ namespace BrowserForSlowNetwork
     static class CoreNetzwerk_
     {
         public static string URL_ = "";
-        private static string punkte2_ = "..";
+        private static string Space = "";
 
+        public static string GetSite(string URI)
+        {
+            GetNameserver(URI);
+            GetSite();
+            return Space;
+        }
 
-        public static string NameServerIP_ = "http://alexmitter.tk/tknameserver/standart.tkn";
-        public static string NameServerFileSpace_;
-        public static string NameServerBekommenURI_;
-        public static void Aufrufen_()
+        static string adress;
+        static string[] adress_alone;
+        static string GetNameserver(string URI)
         {
 			/* 
 
-			Networkcode by PlaySteph310
+			Networkcode by PlaySteph310, with Magic by Alexmitter
 
 			*/
-			string adress = CoreClass.Eingabe;
-			string nameserver = "";
+            adress = URI;
 			// remove http://
 			if (adress.Contains("http://") == true)
 			{
@@ -35,20 +39,26 @@ namespace BrowserForSlowNetwork
 				// senseless code by Alexmitter > Console.WriteLine("Hello World");
 			}
 			// split link to single strings
-			string[] adress_alone = adress.Split ('/');
+			adress_alone = adress.Split ('/');
 			// check domain
-			new System.Net.WebClient().DownloadString("http://tk.steph.cf/dns.php?name=" + adress_alone[0]);
-			if (nameserver != "")
-			{
-				for (int i = adress.Length; i > 1; i--)
-				{
-					adress = adress + "/" + adress_alone[i];
-				}
-				CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL_);
-			}
+            return adress_alone[0];
         }
 
-        public static void GetSite(string name, string URL)
+        static void GetSite()
+        {
+            string nameserver = "";
+            new System.Net.WebClient().DownloadString("http://tk.steph.cf/dns.php?name=" + adress_alone[0]);
+            if (nameserver != "")
+            {
+                for (int i = adress.Length; i > 1; i--)
+                {
+                    adress = adress + "/" + adress_alone[i];
+                }
+                Space = new System.Net.WebClient().DownloadString(URL_);
+            }
+        }
+
+        public static void GetSiteManuall(string name, string URL)
         {
             string URI = URL + "/" + name + ".tk";
             CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL);
