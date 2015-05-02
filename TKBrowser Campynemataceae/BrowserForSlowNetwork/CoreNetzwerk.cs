451aabbfb664a486bac6b2fbcbeb11262ef90105
Alexmitter <compaq@live.de>
2015-05-02 13:46:50 +0200

    Ver 0.20.3
    Neue GetSite funktion

--- TKBrowser Campynemataceae/BrowserForSlowNetwork/CoreNetzwerk.cs
+++ TKBrowser Campynemataceae/BrowserForSlowNetwork/CoreNetzwerk.cs
@@ -46,145 +46,157 @@ namespace BrowserForSlowNetwork
 				}
 				CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL);
 			}
+        }
 
-
-
-
-
-
-
-
-            /*StandartPages.EingangsSwitchString = CoreClass.Eingabe + "";
-            StandartPages.EingangsSwitch();
-
-            if (CoreClass.Eingabe == "")
-            {
-                CoreClass.Eingabe = "index";
-            }
-            else if (CoreClass.Eingabe == "Lesezeichen" | CoreClass.Eingabe == "lesezeichen")
-            {
-                Lesezeichen.Menü();
-            }
-            else
-            {
-                Parallel.Invoke(new Action[]{()=>{
-                                                     if(!File.Exists(Path.Combine("sites", CoreClass.Eingabe) + ".tk")) {
-                                                         Ping Sender = new Ping();
-                                                         PingReply Result = Sender.Send("192.168.0.106");
-                                                         if (Result.Status == IPStatus.Success)
-                                                         {
-                                                             URL = "http://192.168.0.106/tkbrowser/" + CoreClass.Eingabe + ".tk";
-                                                         }
-                                                         else
+        public static void GetSite(string name, string URL)
         {
+            string URI = URL + "/" + name + ".tk";
+            CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL);
 
         }
-                                                         {
-                                                             Ping Sender2 = new Ping();
-                                                             PingReply Result2 = Sender.Send("alexmitter.tk");
-                                                             if (Result2.Status == IPStatus.Success)
-                                                             {
-                                                                 URL = "http://alexmitter.tk/tkbrowser/" + CoreClass.Eingabe + ".tk";
-                                                             }
-                                                             else
-                                                             {
-                                                                 Console.WriteLine("    ╔════════════════════════════════════════════════════════════════════════╗");
-                                                                 Console.WriteLine("    ║         Der Client kann sich nicht mit dem Server verbinden            ║");
-                                                                 Console.WriteLine("    ║ Prüfen sie ihre Internetverbindung und versuchen sie es später nochmal ║");
-                                                                 Console.WriteLine("    ╚════════════════════════════════════════════════════════════════════════╝");
-                                                                 CoreClass.zähler = 1;
-                                                                 CoreClass.tester2 = 1;
-                                                                 Console.ReadLine();
-                                                                 Console.Clear();
-                                                                 Aufrufen();
-                                                             }
-                                                         }}
-                                                         CoreClass.zähler = 1;
-                                                         punkte2 = "..";
-                },()=>{
-                          CoreClass.zähler = 0;
-                          System.Threading.Thread.Sleep(500);
-                          Console.Clear();
-                          Console.Title = "TK Browser :: Anpingen...";
-                          while(CoreClass.zähler == 0)
-                          {
-                              punkte2 += ".";
-                              Console.WriteLine("Adresse wird angepingt"+punkte2);
-                              System.Threading.Thread.Sleep(1000);
-                              if (CoreClass.zähler == 0) {
-                                  Console.Clear();
-                              }
-                          }
-                          punkte2 = "..";
-                          Console.Title = "";
-                }});
-            }
-        }
 
-        
-        public static void Downloader()
-        {
-            SoundPlayer player = new SoundPlayer("modem.wav");
-            Parallel.Invoke(new Action[]{()=>{
-                                                 try
-                                                 {
-                                                     var localfilename = Path.Combine("sites",  CoreClass.Eingabe) + ".tk";
-                                                     if(File.Exists(localfilename)) {
-                                                        CoreClass.FileSpace = File.ReadAllText(localfilename);
-                                                     }else {
-                                                        CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL);
-                                                     }                                   
-                                                     Console.Clear();
-                                                     Console.Title = "";
-                                                     Console.ForegroundColor = ConsoleColor.Green;                                                     
-                                                     OutputBox.Nachricht(">> Datei: " + CoreClass.Eingabe);
-                                                     OutputBox.Ausgabe();
-                                                     Console.ForegroundColor = ConsoleColor.Gray;
-                                                     Console.WriteLine("");
-                                                 }
-                                                 catch
-                                                 {
-                                                     if(CoreClass.tester2 == 0)
-                                                     {
-                                                         Console.Clear();
-                                                         Console.Title = "Telekom Browser :: Fehler";
-                                                         Console.WriteLine("");
-                                                         Console.WriteLine("    ╔═════════════════════════════════════════════════════════════════════╗");
-                                                         Console.WriteLine("    ║                Die Datei konnte nicht gefunden werden               ║");
-                                                         Console.WriteLine("    ║                  Bitte überprüfen sie ihre Eingabe                  ║");
-                                                         Console.WriteLine("    ╚═════════════════════════════════════════════════════════════════════╝");
-                                                         player.Stop();
-                                                         CoreClass.zähler = 1;
-                                                         CoreClass.tester = 1;
-                                                         Console.ReadLine();
-                                                         Console.Clear();
-                                                         Aufrufen();
-                                                     }
-                                                 }
-                                                 CoreClass.zähler = 1;
-            },()=>{
-                      CoreClass.zähler = 0;
-                      System.Threading.Thread.Sleep(500);
-                      if (CoreClass.tester == 0)
-                      {
-                          Console.Clear();
-                      }
-                      player.Play();
-                      Console.Title = "Telekom Browser :: Verbinden...";
-                      while(CoreClass.zähler == 0)
-                      {
-                          punkte2 += ".";
-                          Console.WriteLine("Verbindung wird aufgebaut"+punkte2);
-                          System.Threading.Thread.Sleep(1000);
-                          if (CoreClass.zähler == 0) 
-                          {
-                              Console.Clear();
-                          }
-                      }
-                      player.Stop();
-                      punkte2 = "..";
-                      Console.Title = "";
-            }});*/
-        }
+
     }
 }
+
+
+
+
+// Aler Netzwerk Code von Alexmitter
+
+
+
+
+
+
+/*StandartPages.EingangsSwitchString = CoreClass.Eingabe + "";
+StandartPages.EingangsSwitch();
+
+if (CoreClass.Eingabe == "")
+{
+    CoreClass.Eingabe = "index";
+}
+else if (CoreClass.Eingabe == "Lesezeichen" | CoreClass.Eingabe == "lesezeichen")
+{
+    Lesezeichen.Menü();
+}
+else
+{
+    Parallel.Invoke(new Action[]{()=>{
+                                         if(!File.Exists(Path.Combine("sites", CoreClass.Eingabe) + ".tk")) {
+                                             Ping Sender = new Ping();
+                                             PingReply Result = Sender.Send("192.168.0.106");
+                                             if (Result.Status == IPStatus.Success)
+                                             {
+                                                 URL = "http://192.168.0.106/tkbrowser/" + CoreClass.Eingabe + ".tk";
+                                             }
+                                             else
+{
+
+}
+                                             {
+                                                 Ping Sender2 = new Ping();
+                                                 PingReply Result2 = Sender.Send("alexmitter.tk");
+                                                 if (Result2.Status == IPStatus.Success)
+                                                 {
+                                                     URL = "http://alexmitter.tk/tkbrowser/" + CoreClass.Eingabe + ".tk";
+                                                 }
+                                                 else
+                                                 {
+                                                     Console.WriteLine("    ╔════════════════════════════════════════════════════════════════════════╗");
+                                                     Console.WriteLine("    ║         Der Client kann sich nicht mit dem Server verbinden            ║");
+                                                     Console.WriteLine("    ║ Prüfen sie ihre Internetverbindung und versuchen sie es später nochmal ║");
+                                                     Console.WriteLine("    ╚════════════════════════════════════════════════════════════════════════╝");
+                                                     CoreClass.zähler = 1;
+                                                     CoreClass.tester2 = 1;
+                                                     Console.ReadLine();
+                                                     Console.Clear();
+                                                     Aufrufen();
+                                                 }
+                                             }}
+                                             CoreClass.zähler = 1;
+                                             punkte2 = "..";
+    },()=>{
+              CoreClass.zähler = 0;
+              System.Threading.Thread.Sleep(500);
+              Console.Clear();
+              Console.Title = "TK Browser :: Anpingen...";
+              while(CoreClass.zähler == 0)
+              {
+                  punkte2 += ".";
+                  Console.WriteLine("Adresse wird angepingt"+punkte2);
+                  System.Threading.Thread.Sleep(1000);
+                  if (CoreClass.zähler == 0) {
+                      Console.Clear();
+                  }
+              }
+              punkte2 = "..";
+              Console.Title = "";
+    }});
+}
+}
+
+        
+public static void Downloader()
+{
+SoundPlayer player = new SoundPlayer("modem.wav");
+Parallel.Invoke(new Action[]{()=>{
+                                     try
+                                     {
+                                         var localfilename = Path.Combine("sites",  CoreClass.Eingabe) + ".tk";
+                                         if(File.Exists(localfilename)) {
+                                            CoreClass.FileSpace = File.ReadAllText(localfilename);
+                                         }else {
+                                            CoreClass.FileSpace = new System.Net.WebClient().DownloadString(URL);
+                                         }                                   
+                                         Console.Clear();
+                                         Console.Title = "";
+                                         Console.ForegroundColor = ConsoleColor.Green;                                                     
+                                         OutputBox.Nachricht(">> Datei: " + CoreClass.Eingabe);
+                                         OutputBox.Ausgabe();
+                                         Console.ForegroundColor = ConsoleColor.Gray;
+                                         Console.WriteLine("");
+                                     }
+                                     catch
+                                     {
+                                         if(CoreClass.tester2 == 0)
+                                         {
+                                             Console.Clear();
+                                             Console.Title = "Telekom Browser :: Fehler";
+                                             Console.WriteLine("");
+                                             Console.WriteLine("    ╔═════════════════════════════════════════════════════════════════════╗");
+                                             Console.WriteLine("    ║                Die Datei konnte nicht gefunden werden               ║");
+                                             Console.WriteLine("    ║                  Bitte überprüfen sie ihre Eingabe                  ║");
+                                             Console.WriteLine("    ╚═════════════════════════════════════════════════════════════════════╝");
+                                             player.Stop();
+                                             CoreClass.zähler = 1;
+                                             CoreClass.tester = 1;
+                                             Console.ReadLine();
+                                             Console.Clear();
+                                             Aufrufen();
+                                         }
+                                     }
+                                     CoreClass.zähler = 1;
+},()=>{
+          CoreClass.zähler = 0;
+          System.Threading.Thread.Sleep(500);
+          if (CoreClass.tester == 0)
+          {
+              Console.Clear();
+          }
+          player.Play();
+          Console.Title = "Telekom Browser :: Verbinden...";
+          while(CoreClass.zähler == 0)
+          {
+              punkte2 += ".";
+              Console.WriteLine("Verbindung wird aufgebaut"+punkte2);
+              System.Threading.Thread.Sleep(1000);
+              if (CoreClass.zähler == 0) 
+              {
+                  Console.Clear();
+              }
+          }
+          player.Stop();
+          punkte2 = "..";
+          Console.Title = "";
+}});*/
