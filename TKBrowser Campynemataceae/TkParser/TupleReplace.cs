1453d56b759eb8971b38e530c7b7ca948ba80132
Jonathan <dr4yyee@gmail.com>
2015-05-04 22:49:11 +0200

    added parser of the tkbrowsernext

---
+++ TKBrowser Campynemataceae/TkParser/TupleReplace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TkParser
+{
+    static class TupleReplace
+    {
+        public static string Replace(this string subject, params Tuple<string, string>[] replacements)
+        {
+            foreach (var replacement in replacements) { subject = subject.Replace(replacement.Item1, replacement.Item2); }
+            return subject;
+        }
+    }
+}
